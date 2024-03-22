26c5405e15d3725779c28abc53a0bbc534e9161d
muhammadfahid12 <ranafahadirshad@gmail.com>
2024-03-22 21:09:40 +0500

    JWT authentication

---
+++ README.md
@@ -0,0 +1,3 @@
+### JWT is bearer token.
+
+### bcrypt library is used to hash and also compare current password with hash.
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,58 +1,87 @@
-import mongoose, {Schema} from "mongoose";
+  import mongoose, { Schema } from "mongoose";
+  import { Jwt } from "jsonwebtoken";
+  import {bcrypt} from "bcrypt"
 
-const userSchema = new Schema({
-
-    username :{
-        type : String,
+  const userSchema = new Schema(
+    {
+      username: {
+        type: String,
         required: true,
         unique: true,
         lowercase: true,
         trim: true,
-        index: true,    
-    },
+        index: true,
+      },
 
-    email:{
-        type : String,
+      email: {
+        type: String,
         required: true,
         unique: true,
         lowercase: true,
         trim: true,
-    },
-    fullname:{
-        type : String,
+      },
+      fullname: {
+        type: String,
         required: true,
         trim: true,
         index: true,
-    },
-    avatar:{
-        type : String, //cloudinary url 
+      },
+      avatar: {
+        type: String, //cloudinary url
         required: true,
-    },
-    coverImage : {
+      },
+      coverImage: {
         type: String,
-    },
-    watchHistory:[
+      },
+      watchHistory: [
         {
-            type: Schema.Types.ObjectId,
-            ref: "Video"
-        }
-    ],
-    password: {
-        type :String,
-        required: [true, "Password is required"]
-    },
-    refreshToken: {
+          type: Schema.Types.ObjectId,
+          ref: "Video",
+        },
+      ],
+      password: {
         type: String,
+        required: [true, "Password is required"],
+      },
+      refreshToken: {
+        type: String,
+      },
+    },
+    {
+      timestamps: true,
     }
+  );
+  
+  // password Hashing  
 
-},
-{
-    timestamps: true
-}
-)
+  userSchema.pre("save",async function (next){
+      if(!this.isModified("password")) return next();
+      this.password = bcrypt.hash(this.password,10)
+      next()
+  })
 
+  //password validation
 
+  //methods is used to create custom methods
+  userSchema.methods.isPasswordCorrect = async function (password){
+    return await bcrypt.compare(password, this.password)
+  }
 
+  userSchema.methods.generateRefreshTokens = function ()
+  {
+    jwt.sign(
+      {
+        
+        _id : this._id,
+        email : this.email,
+        username: this.username,
+        fullname: this.fullname
+    },
+    process.env.ACCESS_KEY_TOKEN,
+    {
+      expiresIn : process.env.ACCESS_TOKEN_EXPIRY
+    }
+    )
+  }
 
-
-export const User = mongoose.model("User", userSchema);
+  export const User = mongoose.model("User", userSchema);
