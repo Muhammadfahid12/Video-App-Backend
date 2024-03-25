43fde58b69478c4eab6d2cd71b32d6de6954fda0
muhammadfahid12 <ranafahadirshad@gmail.com>
2024-03-25 19:56:50 +0500

    setting of cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,24 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUDNARY_API_KEY,
+  api_secret: process.env.CLOUDNARY_SECRET_KEY,
+});
+
+// if localFilePath is having issues, we'll unlink it from our local server
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    console.log("File is uploaded successfully");
+    console.log(response);
+  } catch (error) {
+    fs.unlinkSync(localFilePath);
+    return null;
+  }
+};
