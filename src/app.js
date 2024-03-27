4a6e793d49d5e49ca3611cb989c4557249a53ad1
muhammadfahid12 <ranafahadirshad@gmail.com>
2024-03-28 02:44:49 +0500

    added controllers

--- src/app.js
+++ src/app.js
@@ -1,29 +1,26 @@
-import express from "express"
-import cors from "cors"
-import cookieParser from "cookie-parser"
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-const app = express()
-
+const app = express();
 
 //use method is mostly used when dealing with some kind of middlewares
-app.use(cors({
-    origin : process.env.CORS_ORIGIN,
-    credentials : true,
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
 
-})
-) 
-
-app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({extended:true, limit: "16kb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
-
-
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
 //using routes
 
-import userRouter from "./routes/user.routes.js"
+import userRouter from "./routes/user.routes.js";
 
-app.use("/api/users", userRouter)
+app.use("/api/users", userRouter);
 
-export {app}
+export { app };
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,80 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+const registerUser = asyncHandler(async (req, res) => {
+  const { username, fullname, email, password } = req.body;
 
-const registerUser = asyncHandler( async (req, res) => {
-    res.status(200).json({
-        message:"ok"
+  if (
+    [username, fullname, email, password].some((field) => {
+      field?.trim() === "";
     })
-})
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
 
-export {registerUser}
+  //check the user alread registered
+
+  const existedUser = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (existedUser) {
+    console.log(existedUser);
+    throw new ApiError(409, "User Already Registerd");
+  }
+
+
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  if(!avatarLocalPath)
+  {
+    throw new ApiError(400, "Avatar file is required")
+  }
+
+  //uplaod on cloudinary
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+  if(!avatar)
+  {
+    throw new ApiError(400, "Avatar is not uploaded")
+  }
+
+  //saving to db
+
+ const user = await User.create({
+    fullname,
+    username,
+    avatar:avatar.url,
+    coverImage:coverImage?.url || "",
+    email,
+    password,
+    username:username.toLowerCase()
+     
+
+  })
+
+  //we will unselect password and refreshToken
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  )
+
+  if(!createdUser)
+  {
+    throw new ApiError(500, "Something went wrong while registering user")
+  }
+
+
+  return res.status(201).json(
+    new ApiResponse(200, createdUser, "user registered successfully") 
+  )
+
+
+});
+
+export { registerUser };
